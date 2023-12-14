23f2fe678a5749ffaa5aa5be7cf69f176351c32d
Aswin Bharath <aswinbharathunof@gmail.com>
2023-12-14 21:37:43 +0530

    Database Changed

--- NaturesFurniture/settings.py
+++ NaturesFurniture/settings.py
@@ -87,17 +87,34 @@ WSGI_APPLICATION = 'NaturesFurniture.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Nature\'s Furniture',
+#         'USER': 'aswinbharath',
+#         'PASSWORD': '435694',
+#         'HOST':'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Nature\'s Furniture',
-        'USER': 'aswinbharath',
-        'PASSWORD': '435694',
-        'HOST':'localhost',
-        'PORT': '5432',
-    }
-}
 
+   					 'default': {
+
+       					 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        				 'NAME': 'myproject',
+
+        				 'USER': 'myprojectuser',
+
+        				 'PASSWORD': 'password',
+
+       					 'HOST': 'localhost',
+
+        				 'PORT': '',
+
+    						}
+					}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -135,6 +152,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
